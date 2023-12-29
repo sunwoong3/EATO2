85d34730be9a4ac3c542953aad87ecd9aa259ac7
sunwoo <sunw339@gmail.com>
2023-12-29 15:19:30 +0900

    mongodb 연결 추가

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import routes from "#src/routes/index.js";
 
+dotenv.config();
+
 const app = express();
-const port = 3002;
+const { PORT, MONGO_URI } = process.env;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+await mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("Successfully connected to mongodb"))
+  .catch((e) => console.error(e));
+
+app.listen(PORT, () => {
+  console.log(`EATO2 listening on PORT ${PORT}`);
 });
