d7a21fd26a6acfbb1e423f5a69cac9c5f3ac64ab
sunwoo <sunw339@gmail.com>
2024-01-02 12:30:52 +0900

    [server] cors 추가

--- server/src/app.js
+++ server/src/app.js
@@ -2,17 +2,19 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import routes from "#src/routes/index.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const { PORT, MONGO_URI } = process.env;
 
+app.use(cors({ origin: "*" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-await mongoose
+https: await mongoose
   .connect(MONGO_URI)
   .then(() => console.log("Successfully connected to mongodb"))
   .catch((e) => console.error(e));
