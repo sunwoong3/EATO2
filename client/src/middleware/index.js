164355b6b35c17fe0a8ea65f9b7ba1a757036b67
sunwoo <sunw339@gmail.com>
2024-01-03 15:54:13 +0900

    [client] 양방향 암호화, 복호화 함수 생성

--- client/src/middleware/index.js
+++ client/src/middleware/index.js
@@ -1,6 +1,23 @@
+import CryptoJS from "crypto-js";
+
 const encryptPwd = (data) => {
-  console.log(data, "encryptPwd");
-  return data;
+  let { password } = data;
+  password = CryptoJS.AES.encrypt(process.env.SECRET_KEY, password).toString();
+
+  const encryptData = Object.assign(data, { password });
+
+  return encryptData;
 };
 
-export { encryptPwd };
+const decryptPwd = (data) => {
+  let { password } = data;
+  password = CryptoJS.AES.decrypt(process.env.SECRET_KEY, password).toString(
+    CryptoJS.enc.Utf8
+  );
+
+  const decryptData = Object.assign(data, { password });
+
+  return decryptData;
+};
+
+export { encryptPwd, decryptPwd };
