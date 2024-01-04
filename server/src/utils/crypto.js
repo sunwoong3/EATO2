441e6c1536958beb78aa50afbac6a099167e9ba2
sunwoo <sunw339@gmail.com>
2024-01-04 18:33:09 +0900

    [server] encrypt decrypt 함수 수정

--- server/src/utils/crypto.js
+++ server/src/utils/crypto.js
@@ -2,18 +2,16 @@ import CryptoJS from "crypto-js";
 
 const encryptPwd = (password) => {
   const encryptPwd = CryptoJS.AES.encrypt(
-    process.env.SECRET_KEY,
-    password
+    password,
+    process.env.SECRET_KEY
   ).toString();
 
   return encryptPwd;
 };
 
 const decryptPwd = (password) => {
-  const decryptPwd = CryptoJS.AES.decrypt(
-    process.env.SECRET_KEY,
-    password
-  ).toString(CryptoJS.enc.Utf8);
+  let decryptPwd = CryptoJS.AES.decrypt(password, process.env.SECRET_KEY);
+  decryptPwd = decryptPwd.toString(CryptoJS.enc.Utf8);
 
   return decryptPwd;
 };
