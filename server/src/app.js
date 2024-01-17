c887aafe1462af1b3a84b62b8109862134722b95
sunwoo <sunw339@gmail.com>
2024-01-17 18:48:18 +0900

    [server] 소켓 - 작업중

--- server/src/app.js
+++ server/src/app.js
@@ -4,21 +4,31 @@ import dotenv from "dotenv";
 import routes from "#src/routes/index.js";
 import cors from "cors";
 
+import { socket } from "./socket.js";
+
 dotenv.config();
 
 const app = express();
+const socketOptions = {};
+const corsOption = {
+  origin: "http://localhost:3002",
+  credentials: true,
+};
+
 const { PORT, MONGO_URI } = process.env;
 
-app.use(cors({ origin: "http://localhost:3002", credentials: true }));
+app.use(cors(corsOption));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-https: await mongoose
+await mongoose
   .connect(MONGO_URI)
   .then(() => console.log("Successfully connected to mongodb"))
   .catch((e) => console.error(e));
 
-app.listen(PORT, () => {
+let server = app.listen(PORT, () => {
   console.log(`EATO2 listening on PORT ${PORT}`);
 });
+
+socket(server, socketOptions, app);
